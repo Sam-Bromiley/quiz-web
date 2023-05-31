948cdbc815155facc1134dab2503042795ac1935
Sam Bromiley <sam.bromiley@IRIS.CO.UK>
2023-05-31 13:19:37 +0100

    changed back-end URL

--- src/services/config.service.js
+++ src/services/config.service.js
@@ -2,7 +2,7 @@ angular.module('app').factory('ConfigService', [function() {
 
     var service = {};
 
-    service.apiBase = 'http://localhost:8001/';
+    service.apiBase = 'http://quiz-api-sagb-env.eba-uwcsxev2.eu-west-2.elasticbeanstalk.com/quiz/';
 
     return service;
 }]);
